0c46796980de0e4694bfbe91baed1c8a0271decf
s0177158 <144150068+s0177158@users.noreply.github.com>
2023-11-13 00:44:58 +0300

    8

--- web/web1/my.js
+++ web/web1/my.js
@@ -82,4 +82,33 @@ function change() {
         return 0;
   }
 
+  document.getElementById("openFormButton").addEventListener("click", function() {
+    document.getElementById("popupForm").style.display = "block";
+    history.pushState({ formOpen: true }, '', '/s0177158.github.io');
+    
+  });
+  
 
+
+  document.getElementById("feedbackForm").addEventListener("submit", function(event) {
+    var slapform = new Slapform();
+slapform.submit({
+form: 'h1g3zZhDr',
+data: {
+name: '',
+message: '',
+}
+})
+    .then(function (response) {
+    console.log('Success', response)
+    })
+    .catch(function (e) {
+    console.error('Fail', e)
+    })
+
+    document.getElementById("popupForm").style.display = "none"; 
+  });
+  function closePopup() {
+    document.getElementById("popupForm").style.display = "none";
+    window.location.href = "s0177158.github.io/web/web1/number1.html";
+  }
